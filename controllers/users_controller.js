1b121ba1a7ac49f5b15231a28a9842be3c53b719
Mohit Saini <mohit@gmail.com>
2023-04-30 21:20:24 +0530

    To data autherization

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,12 +1,27 @@
 const User = require('../models/user');
 
 
+module.exports.profile = async (req, res) =>{
+     try{
+          if(req.cookies.user_id){
+               const user = await User.findById(req.cookies.user_id);
+               if(user){
+                    return res.render('user_profile', {
+                         title: "User Profile",
+                         user: user
+                    });
+               }
+          }
+          else{
+               return res.redirect('/users/sign-In');
+          }
 
-module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title : "User Profile"
-    });
+     } catch(err){
+          console.log('error in creating user');
+          return;
+     }
 }
+
 
 // render the sign up page
 module.exports.signUp = function(req,res){
@@ -23,29 +38,54 @@ module.exports.signIn = function(req,res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
+module.exports.create = async (req, res)=>{
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
-
-    User.findOne({email: req.body.email}, async function(err,user){
-        if (err){console.log('Error in finding user in signing up');return}
-        if(!user){
-            let userCreate = await User.create(req.body, async function(err,user){
-                if (err){
-                    console.log('Error in finding user while signing up');return}
-                    
-                return res.redirect('/users/SignIn');
-            })
-        }else{
-            return res.redirect('back');
-        }
-    });
+    // finding one email id is present in DB already or not
+    const user = await User.findOne({email: req.body.email});
+     if(!user) {
+          try {
+               User.create(req.body);
+               return res.redirect('/users/sign-In');
+          }
+          catch(err) {
+               console.log('error in creating user');
+               return;
+          } 
+     }
+     else {
+          return res.redirect('back');
+     }
 }
 
 
 
 // get the sign in  session for user
-module.exports.create_session = function(req, res){
-    // TODO later
-}
+module.exports.create_session = async (req, res)=>{
+    // steps to authenticate
+
+    // find the user
+    const user = await User.findOne({email: req.body.email});
+     //handle user found
+     if(user) {
+          try {
+               //handle password which don't match
+               if(user.password != req.body.password){
+                    return res.redirect('back');
+               }
+               //handle session creation
+               res.cookie('user_id', user.id);
+               return res.redirect('/users/profile');
+          }
+          catch(err) {
+               console.log('error in creating user');
+               return;
+          } 
+     }
+     //handle user not found
+     else {
+          return res.redirect('/users/sign-Up');
+     }
+    
+    }
--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,13 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile',usersController.profile);
 
-router.get('/SignUp',usersController.signUp);
+router.get('/sign-Up',usersController.signUp);
 
-router.get('/SignIn',usersController.signIn);
+router.get('/sign-In',usersController.signIn);
 
 router.post('/create',usersController.create);
 
-
+router.post('/create_session',usersController.create_session);
 
 
 module.exports = router;
