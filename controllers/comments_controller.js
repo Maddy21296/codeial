14637ce3fb60c970cc7ec1768d73e2cbe83f3dce
Mohit Saini <mohit@gmail.com>
2023-05-16 20:16:10 +0530

    Deleting comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,29 @@ module.exports.create = async (req, res) => {
         return res.redirect('back');
         }
 }
+
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+
+            let postId = comment.post;
+
+            comment.remove();
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+
+            return res.redirect('back');
+        }else{
+            return res.redirect('back');
+        }
+    }catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
+    
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -18,19 +18,20 @@ module.exports.create = async (req, res) => {
 }
 
 
-module.exports.destroy = async (req, res) => {
+module.exports.destroy = async function (req, res) {
 
     try {
         let post = await Post.findById(req.params.id);
         // .id means converting the object id into string
         if (post.user == req.user.id) {
 
-            // CHANGE :: delete the associated likes for the post and all its comments' likes too
+
             post.remove();
 
             await Comment.deleteMany({ post: req.params.id });
 
             return res.redirect('back');
+
         } else {
             return res.redirect('back');
         }
