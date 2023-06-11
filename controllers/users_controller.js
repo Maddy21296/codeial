14a9e0bd8e8dc2c70425d01fd91d6ac46d82e578
Mohit Saini <mohit@gmail.com>
2023-06-11 16:24:37 +0530

    Making API using postman

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -12,18 +12,58 @@ module.exports.profile = async function(req, res){
 
 
 module.exports.update = async function(req,res){
-  if(req.user.id == req.params.id){
-    try{
-      let user = await User.findByIdAndUpdate(req.params.id,req.body);
+//   if(req.user.id == req.params.id){
+//     try{
+//       let user = await User.findByIdAndUpdate(req.params.id,req.body);
       
-        return res.redirect('back');
+//         return res.redirect('back');
       
-    }catch(err){
+//     }catch(err){
+//       req.flash('error', err);
+//       return res.redirect('back');
+//     }
+//   }else{
+//     req.flash('error', 'Unauthorized!');
+//     return res.status(401).send('Unauthorized');
+// }
+// }
+if(req.user.id == req.params.id){
+
+  try{
+
+      let user = await User.findById(req.params.id);
+      User.uploadedAvatar(req, res, function(err){
+          if (err) {console.log('*****Multer Error: ', err)}
+          
+          user.name = req.body.name;
+          user.email = req.body.email;
+
+          if (req.file){
+
+              // if (user.avatar){
+              //     fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+              // }
+
+
+              // this is saving the path of the uploaded file into the avatar field in the user
+              user.avatar = User.avatarPath + '/' + req.file.filename;
+          }
+          user.save();
+          return res.redirect('back');
+      });
+
+  }catch(err){
       req.flash('error', err);
       return res.redirect('back');
-    }
   }
+
+
+}else{
+  req.flash('error', 'Unauthorized!');
+  return res.status(401).send('Unauthorized');
 }
+}
+
 
 
 
@@ -80,10 +120,11 @@ module.exports.create_session = function(req, res){
 }
 
 module.exports.destroySession = function(req,res){
-  req.logout(function(req){
-    req.flash('success', 'You have logged out!');
-    return;
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Signed out Successfully");
+    return res.redirect("/users/SignIn");
   });
-  
-  return res.redirect('/');
 }
--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');// declaring multer to access multipart of uploading files
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars'); // path where we are going to store in server
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -14,10 +18,27 @@ const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required: true
+    },
+    avatar:{
+        type: String // field which will be storing the path of the file coz database just stores the path of file not complete file
     }
 },{
     timestamps: true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  });
+
+//static methods
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User',userSchema);
 
